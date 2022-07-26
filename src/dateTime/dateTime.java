ee7afc312e6a35f3d9f103126e7d5be9b3ad484a
Ryan Coslove <ryancoslove@gmail.com>
2022-07-25 22:41:11 -0400

    Create initial files and methods - read and write a file

--- src/dateTime/dateTime.java
+++ src/dateTime/dateTime.java
@@ -1,54 +1,54 @@
 package dateTime;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class dateTime {
 
-	static void readFile() throws FileNotFoundException {
-		File file = new File("C:\\Users\\ryanc\\eclipse-workspace\\dateTime\\testFile.txt");
-		Scanner sc = new Scanner(file);
-		try {
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
-		} finally {
-			sc.close();
-		}
-	}
-
-	static void createFile() {
-		try {
-			File output = new File("validDateTimes.txt");
-			if (output.createNewFile()) {
-				System.out.println("File created: " + output.getName());
-			} else {
-				System.out.println("File already exists.");
-			}
-		} catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-	}
-
-	static void writeFile() {
-		try {
-			FileWriter myWriter = new FileWriter("validDateTimes.txt");
-			myWriter.write("Files in Java might be tricky, but it is fun enough!");
-			myWriter.close();
-			System.out.println("Successfully wrote to the file.");
-		} catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		readFile();
-		createFile();
-		writeFile();
-	}
+	// Main driver method
+    public static void main(String[] args)
+    {
+        // Try block to check for exceptions
+        try {
+ 
+            // Creating a FileReader object 
+            FileReader fr = new FileReader("testFile.txt");
+ 
+            // Creating a FileWriter object
+            FileWriter fw = new FileWriter("validDateTimes.txt");
+ 
+            // Declaring a blank string in which content of file will be stored
+            String str = "";
+ 
+            int i;
+ 
+            // read() method will read the file char by char
+            // EOF = -1
+            while ((i = fr.read()) != -1) {
+                // Storing every character in the string
+                str += (char)i;
+            }
+ 
+            // Print and display the string that contains file data
+            System.out.println(str);
+ 
+            // Write string to write object
+            fw.write(str);
+ 
+            // close streams
+            fr.close();
+            fw.close();
+ 
+            // Display message
+            System.out.println(
+                "File reading and writing both successful");
+        }
+ 
+        // Catch block to handle the exception like no file in path
+        catch (IOException e) {
+            // Display message
+            System.out.println("There are some IOException" + e);
+        }
+    }
 }
