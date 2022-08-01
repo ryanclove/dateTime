237769931c3538021f8b5f34c6df25e72cf53d10
Ryan Coslove <ryancoslove@gmail.com>
2022-07-31 22:08:11 -0400

    Checks for duplicates and writes to file

--- src/dateTime/dateTime.java
+++ src/dateTime/dateTime.java
@@ -1,53 +1,71 @@
 package dateTime;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class dateTime {
 
+	public boolean validDateFormat(String str) {
+		// ISO 8601 YYYY-MM-DDThh:mm:ssTZD
+		SimpleDateFormat iso = new SimpleDateFormat("YYYY-MM-DDThh:mm:ssTZD");
+
+		return false;
+	}
+
 	// Main driver method
-    public static void main(String[] args)
-    {
-        // Try block to check for exceptions
-        try {
- 
-            // Creating a FileReader object 
-            FileReader fr = new FileReader("testFile.txt");
- 
-            // Creating a FileWriter object
-            FileWriter fw = new FileWriter("validDateTimes.txt");
- 
-            // Declaring a blank string in which content of file will be stored
-            String str = "";
-           
-            try (BufferedReader br = new BufferedReader(fr)) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                   // process the line.
-                	str += line + "\n";
-                }
-            }
- 
-            // Print and display the string that contains file data
-            System.out.println(str);
- 
-            // Write string to write object
-            fw.write(str);
- 
-            // close streams
-            fr.close();
-            fw.close();
- 
-            // Display message
-            System.out.println("File reading and writing both successful");
-        }
- 
-        // Catch block to handle the exception like no file in path
-        catch (IOException e) {
-            // Display message
-            System.out.println(e);
-        }
-    }
+	public static void main(String[] args) {
+		// Try block to check for exceptions
+		try {
+
+			// Creating a FileReader object
+			FileReader fr = new FileReader("testFile.txt");
+
+			// Creating a FileWriter object
+			FileWriter fw = new FileWriter("validDateTimes.txt");
+
+			// Declaring a set because it won't store duplicates
+			Set<String> set = new HashSet<>();
+
+			try (BufferedReader br = new BufferedReader(fr)) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					// process the line, add it to ArrayList.
+					set.add(line);
+				}
+			}
+
+			// Declaring an arrayList to store the Strings from the Set
+			ArrayList<String> list = new ArrayList<String>(set);
+
+			// Write
+			try {
+				// Declaring a blank string str in which content of file will be stored
+				for (String str : list) {
+					// write to file
+					fw.write(str + System.lineSeparator());
+					// Print and display the string that contains file data
+					System.out.println(str);
+				}
+				fw.close();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+
+			// close streams
+			fr.close();
+			fw.close();
+
+			// Display message
+			System.out.println("File reading and writing both successful");
+		}
+
+		// Catch block to handle the exception like no file in path
+		catch (
+
+		IOException e) {
+			// Display message
+			System.out.println(e);
+		}
+	}
 }
